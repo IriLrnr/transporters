6ffd01e71d6e8dbea3ec1a05b25c0d702b8e92a3
Irina Lerner <iri.lerner@gmail.com>
2020-08-14 17:10:23 -0300

    Read file names

--- R/transp.R
+++ R/transp.R
@@ -1,3 +1,14 @@
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+
+setwd("./data")
+file.names <- dir()
+
+number.spp=data.frame()
+
+for (i in 1:(length(file.names)-1)){
+  dados <- read.csv(paste(file.names[i]), head=TRUE, sep=";")
+  number.spp <- rbind(number.spp, dados)
+}
+setwd("../")
