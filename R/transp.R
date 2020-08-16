1a735be0200f0ee4152df5326db0b27aa4ea1b21
Irina Lerner <iri.lerner@gmail.com>
2020-08-16 17:50:03 -0300

    Try to apply edition to all the tables

--- R/transp.R
+++ R/transp.R
@@ -1,14 +1,42 @@
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(gdata)
+library(rlist)
+library(data.table)
+
+read_and_filter <- function (x) {
+  temp <- read.table(x, header = T, sep = "\t", quote="", dec = ".", fill = TRUE)
+  temp <- temp[,c(2, 6, 8)]
+  colnames(temp) <- c("orf", "score", "sig")
+  temp <- subset(temp, temp$sig == "yes", c("orf", "score"))
+  return(temp)
+}
 
 setwd("./data")
 file.names <- dir()
 
-number.spp=data.frame()
+file.names <- file.names[1:10]
+list.all <- lapply(file.names, function(x) read_and_filter)
+                   
+                   FUN = read.table, header = T, sep = "\t", quote = "", fill = T)
 
-for (i in 1:(length(file.names)-1)){
-  dados <- read.csv(paste(file.names[i]), head=TRUE, sep=";")
-  number.spp <- rbind(number.spp, dados)
+list2 <- lapply(list.all, function(x) as.data.frame(subset(x, x[,8] == "yes", select = c(2, 6))))
+
+list.new <- lapply(list.all, FUN = colnames, c("sig"))
+
+list.2 <- lapply(list.all, FUN = subset,  == "yes", c(2, 6)))
+
+sig.compounds.list <- list()
+i = 1
+
+
+for (i in 1:(3)) {
+  temp <- read.table(file.names[i], header = T, sep = "\t", quote="", dec = ".", fill = TRUE)
+  temp <- temp[,c(2, 6, 8)]
+  colnames(temp) <- c("orf", "score", "sig")
+  enriched <- subset(temp, temp$sig == "yes", c("orf", "score"))
+  list.append(sig.compounds.list, enriched)
 }
 setwd("../")
+sig_compounds_list
